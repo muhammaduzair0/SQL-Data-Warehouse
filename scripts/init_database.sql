1723a7a016be08fba976ad643deeed0facb1840f
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-12-23 13:06:36 +0500

    Drop and recreate the 'DataWarehouse' database

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -17,3 +17,12 @@ WARNING:
 USE master;
 GO
 
+-- Drop and recreate the 'DataWarehouse' database
+
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
+END;
+GO
+
