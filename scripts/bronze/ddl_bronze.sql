085356d7c33a2c90af732191a95835c8dfa1efd6
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-12-24 12:18:33 +0500

    Table Created Bronze CRM Customer Info

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -12,3 +12,13 @@ Script Purpose:
 IF OBJECT_ID('bronze.crm.cust.info', 'U') IS NOT NULL
     DROP TABLE bronze.crm_cust_info;
 
+CREATE TABLE bronze.crm_cust_info(
+    cst_id INT,
+    cst_key NVARCHAR(50),
+    cst_firstname NVARCHAR(50),
+    cst_lastname NVARCHAR(50),
+    cst_material_status NVARCHAR(50),
+    cst_gndr NVARCHAR(50),
+    cst_create_date DATE
+);
+
