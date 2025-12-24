ac9d0877e413ce9a76cbf3b75bd2c2b6d235d53d
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-12-24 12:20:25 +0500

    Table Created Bronze ERP location

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -53,3 +53,8 @@ CREATE TABLE bronze.crm_sales_details (
 IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
     DROP TABLE bronze.erp_loc_a101;
 
+CREATE TABLE bronze.erp_loc_a101 (
+    cid NVARCHAR(50),
+    cntry NVARCHAR (50)
+);
+
