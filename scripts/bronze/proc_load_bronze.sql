4a65316e802849159de477cbbd21fe32ea538322
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2026-01-05 12:35:45 +0500

    Declare Batch Start/End Time & Batch Start/End Time

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -19,3 +19,10 @@ Usage Example:
 
 CREATE OR ALTER PROCEDURE bronze.load_bronze
 AS
+BEGIN
+    DECLARE 
+        @start_time DATETIME,
+        @end_time DATETIME,
+        @batch_start_time DATETIME,
+        @batch_end_time DATETIME;
+
