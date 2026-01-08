0aa54f66db3c001f54d51e9a7ac32ef8afb53bb4
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2026-01-08 13:09:01 +0500

    DDL Silver Layer Structure Details

---
+++ scripts/silver/ddl_silver.sql
@@ -0,0 +1,11 @@
+/*
+===============================================================================
+DDL Script: Create Silver Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'bronze' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
+
