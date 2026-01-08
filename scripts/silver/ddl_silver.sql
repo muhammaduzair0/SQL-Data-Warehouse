1f9a0b14e2b87c09714311a40a4c3e58985dc039
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2026-01-08 13:11:56 +0500

    Create Table Silver: CRM customer Info

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -12,3 +12,14 @@ Script Purpose:
 IF OBJECT_ID('silver.crm.cust.info', 'U') IS NOT NULL
     DROP TABLE silver.crm_cust_info;
 
+CREATE TABLE silver.crm_cust_info(
+    cst_id              INT,
+    cst_key             NVARCHAR(50),
+    cst_firstname       NVARCHAR(50),
+    cst_lastname        NVARCHAR(50),
+    cst_material_status NVARCHAR(50),
+    cst_gndr            NVARCHAR(50),
+    cst_create_date     DATE,
+    dwh_create_date     DATETIME2 DEFAULT GETDATE()
+);
+
