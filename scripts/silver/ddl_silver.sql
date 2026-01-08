f6790ff88e86a1071781fbeb69e9b65c461810f7
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2026-01-08 13:14:34 +0500

    Create Table Silver: ERP category g1v2

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -76,3 +76,10 @@ CREATE TABLE silver.erp_cust_az12(
 IF OBJECT_ID('silver.erp_px_cat_g1v2', 'U') IS NOT NULL
     DROP TABLE silver.erp_px_cat_g1v2;
 
+CREATE TABLE silver.erp_px_cat_g1v2(
+    id              NVARCHAR(50),
+    cat             NVARCHAR(50),
+    subcat          NVARCHAR(50),
+    maintenance     NVARCHAR(50),
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
