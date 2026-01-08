46bab32121c41f799177000191a24468a8f3720b
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2026-01-08 13:13:29 +0500

    Check Table Is NOT NULL Then Drop the Table

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -63,3 +63,6 @@ CREATE TABLE silver.erp_loc_a101 (
     dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 
+IF OBJECT_ID('silver.erp_cust_az12', 'U') IS NOT NULL
+    DROP TABLE silver.erp_cust_az12;
+
