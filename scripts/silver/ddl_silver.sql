6002723611cfc51d8320d0f4359b9d3650ce8ebd
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2026-01-08 13:14:05 +0500

    Create Table Silver: ERP customer az12

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -66,3 +66,10 @@ CREATE TABLE silver.erp_loc_a101 (
 IF OBJECT_ID('silver.erp_cust_az12', 'U') IS NOT NULL
     DROP TABLE silver.erp_cust_az12;
 
+CREATE TABLE silver.erp_cust_az12(
+    cid             NVARCHAR(50),
+    bdate           DATE,
+    gen             NVARCHAR(50),
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+
