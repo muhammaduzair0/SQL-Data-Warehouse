e3e993ab1bdde534559bc2a63d00063ecaf74592
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2026-01-12 13:50:52 +0500

    Selecting data erp_cust_az12

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -182,3 +182,23 @@ BEGIN
         bdate,
         gen
     )
+    SELECT
+    CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LEN(cid)) -- Remove 'NAS' prefix if present
+        ELSE cid
+    END AS cid,
+    CASE WHEN bdate > GETDATE() THEN NULL
+        ELSE bdate
+    END AS bdate, -- Set future birthdates to NULL
+    CASE WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
+        WHEN UPPER(TRIM(gen)) IN ('M', 'MALE') THEN 'Male'
+        ELSE 'n/a'
+    END AS gen -- Normalize gender values and handle unknown cases
+    FROM bronze.erp_cust_az12
+    SET @end_time = GETDATE()
+    PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+    PRINT '>> -------------';
+
+	PRINT '------------------------------------------------';
+	PRINT 'Loading ERP Tables';
+	PRINT '------------------------------------------------';
+
