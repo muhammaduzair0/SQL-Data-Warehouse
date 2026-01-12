471c2ae3ac2101ff570a415552b5433c9adf3861
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2026-01-12 13:49:16 +0500

    Select Data silver.crm_prd_info

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -98,3 +98,28 @@ BEGIN
             prd_start_dt,
             prd_end_dt
         )
+    SELECT
+        prd_id,
+        REPLACE (SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id, -- Extract category ID
+        SUBSTRING(prd_key, 7, LEN (prd_key)) AS prd_key, -- Extract product key
+        prd_nm,
+        ISNULL (prd_cost, 0) AS prd_cost,
+        CASE UPPER(TRIM(prd_line))
+            WHEN 'M' THEN 'Mountain'
+            WHEN 'R' THEN 'Road'
+            WHEN 'S' THEN 'Other Sales'
+            WHEN 'T' THEN 'Touring'
+            ELSE 'n/a'
+        END AS prd_line, -- Map product line codes to descriptive values
+        CAST(prd_start_dt AS DATE) AS prd_start_dt,
+        CAST(
+            LEAD (prd_start_dt) OVER (
+                PARTITION BY
+                    prd_key
+                ORDER BY
+                    prd_start_dt
+            ) - 1 AS DATE
+        ) AS prd_end_dt -- Calculate end date as one day before the next start date
+    FROM
+        bronze.crm_prd_info;
+
