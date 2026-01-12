202a40eb19fe66e79bc39e2742a92d1b0885286c
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2026-01-12 13:48:26 +0500

    Cleaning the Data of Silver Layer

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -47,3 +47,40 @@ BEGIN
             cst_gndr,
             cst_create_date
         )
+    SELECT
+        cst_id,
+        cst_key,
+        TRIM(cst_firstname) AS cst_firstname,
+        TRIM(cst_lastname) AS cst_lastname,
+        CASE
+            WHEN UPPER(TRIM(cst_material_status)) = 'S' THEN 'Single'
+            WHEN UPPER(TRIM(cst_material_status)) = 'M' THEN 'Married'
+            ELSE 'n/a'
+        END cst_material_status, -- Normalize marital status values to readable format
+        CASE
+            WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+            WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+            ELSE 'n/a'
+        END cst_gndr, -- Normalize gender values to readable format
+        cst_create_date
+    FROM
+        (
+            SELECT
+                *,
+                ROW_NUMBER() OVER (
+                    PARTITION BY
+                        cst_id
+                    ORDER BY
+                        cst_create_date DESC
+                ) as flag_last
+            FROM
+                bronze.crm_cust_info
+            WHERE
+                cst_id IS NOT NULL
+        ) t
+    WHERE
+        flag_last = 1; -- Select the most recent record per customer
+    SET @end_time = GETDATE();
+    PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds'
+    PRINT '>> -------------';
+
