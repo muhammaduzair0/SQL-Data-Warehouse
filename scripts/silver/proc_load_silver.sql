b8280e2c33f6b5674784299314ff08161f53deff
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2026-01-12 13:49:44 +0500

    Insert into silver.crm_sales_details

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -123,3 +123,15 @@ BEGIN
     FROM
         bronze.crm_prd_info;
 
+    INSERT INTO
+        silver.crm_sales_details (
+            sls_ord_num,
+            sls_prd_key,
+            sls_cust_id,
+            sls_order_dt,
+            sls_ship_dt,
+            sls_due_dt,
+            sls_sales,
+            sls_quantity,
+            sls_price
+        )
